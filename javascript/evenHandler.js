475bd727dd6a526d24283d384b4e4d563cd4eae6
monoronjon sarker <monoronjons86@gmail.com>
2024-07-08 17:31:09 +0600

    primative and reverance add

---
+++ javascript/evenHandler.js
@@ -0,0 +1,14 @@
+//primative and reverance 
+// let x=5;
+// let y=6;
+// x=y;
+// console.log(x);
+// console.log(y);
+// const a=["js","python"]
+
+let a=["js","python"];
+let b=["html","css"];
+b=a;
+ a.push("Go");
+console.log(a);
+console.log(b);
