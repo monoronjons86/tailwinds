fc054f93ee617677c2b93fe20f68395c2191cd26
monoronjon sarker <monoronjons86@gmail.com>
2024-07-08 19:25:34 +0600

    array add

---
+++ javascript/array.js
@@ -0,0 +1,38 @@
+// const fruits=["Bannana","Orange","Lemon","Apple","mango"];
+// const result=fruits.find((f)=>f==="Orange"||f==="Lemon");
+// console.log(result);
+
+// const fruits=["Bannana","Orange","Lemon","Apple","mango"];
+// const result=fruits.filter((f)=>f==="Orange"||f==="Lemon");
+// console.log(result);
+// console.log(fruits);
+
+// const fruits=["Bannana","Orange","Lemon","Apple","mango"];
+// const result=fruits.slice(2,4)
+// console.log(result);
+
+// const fruits=["Bannana","Orange","Lemon","Apple","mango","Apple"];
+// const result=fruits.map(f=>{
+//     if(f=="Apple"){
+//         return "Apple";
+//     }
+//     else{
+//         return"N/A";
+//     }
+// });
+// console.log(result);
+// console.log(fruits);
+
+
+// const number=[2,3,4,5,6];
+// const result=number.reduce((total,currValue)=>total+currValue,0)
+// console.log(result);
+
+
+const numbers=[2,4,5,6,7];
+for(sum of numbers){
+    console.log(sum);
+}
+
+
+
