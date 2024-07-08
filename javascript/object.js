acc79cfd07801fa30d9d33cff1f3c7d9ea8c1f46
monoronjon sarker <monoronjons86@gmail.com>
2024-07-08 20:15:54 +0600

    spreed learn

---
+++ javascript/object.js
@@ -0,0 +1,7 @@
+const language={
+    name:"javascript",
+    year:1995,
+    creator:"Brendan Eich"
+}
+console.log(Object.values(language));
+console.log(Object.keys(language));
---
+++ javascript/spreed.js
@@ -0,0 +1,12 @@
+const fruits=["Bannana","Oranges"];
+// const language={
+//     name:"javascript",
+//     year:1995,
+//     creator:"Brendon Eich"
+// }
+
+const result=[...fruits];
+
+result.push("Pineapple");
+console.log(result);
+console.log(fruits);
